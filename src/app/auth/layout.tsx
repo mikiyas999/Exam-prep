e4cda11d6b262ee8132b9cdd8deb8cb951fa1601
Mikiyas Getachew <mikiyasget1234@gmail.com>
2025-06-22 16:13:15 +0100

    create layout for auth

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,12 @@
+// app/auth/layout.tsx
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
